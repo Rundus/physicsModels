9c22a411bc5fa2a79e55c447c7d521262e726af7
Rundus <connorfeltman@yahoo.com>
2025-01-17 15:54:47 -0600

    physicsModels commit 26

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ exclude_patterns = ['AlfvenWave_Resonance.rst',
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+html_theme = 'alabaster'
 # html_theme = 'pydata_sphinx_theme'
 
 # -- Theme configuration -----------------------------------------------------
