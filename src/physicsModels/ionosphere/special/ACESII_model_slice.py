34b8e8c331b10abeec6240ee11cc5d406e5337bb
Rundus <connorfeltman@yahoo.com>
2025-03-24 11:21:36 -0500

    physicsModels commit 44

--- src/physicsModels/ionosphere/special/ACESII_model_slice.py
+++ src/physicsModels/ionosphere/special/ACESII_model_slice.py
@@ -1,7 +1,9 @@
 # --- ACESII_model_slice.py ---
 # Description: compare the model results to the REAL ACES-II data
 from src.physicsModels.ionosphere.ionization_recombination.ionizationRecomb_classes import *
-def generateIonizationRecomb():
+import time
+start_time = time.time()
+def ACESII_model_slice():
 
     # --- imports ---
     from src.physicsModels.ionosphere.conductivity.conductivity_toggles import conductivityToggles
@@ -19,67 +21,39 @@ def generateIonizationRecomb():
     # get the spatial data dict
     data_dict_conductivity = stl.loadDictFromFile(rf'{conductivityToggles.outputFolder}\conductivity.cdf')
 
-    # get the ACES-II attitude data
-    data_dict_attitude_high = stl.
+    # get the ACES-II L-SHell data
+    data_dict_LShell_high = stl.loadDictFromFile(r'C:\Data\ACESII\science\L_shell\high\ACESII_36359_Lshell.cdf')
+    data_dict_LShell_low = stl.loadDictFromFile(r'C:\Data\ACESII\science\L_shell\low\ACESII_36364_Lshell.cdf')
 
-    ############################
-    # --- PREPARE THE OUTPUT ---
-    ############################
-    LShellRange = data_dict_plasma['simLShell'][0]
-    altRange = data_dict_plasma['simAlt'][0]
-
-    data_dict_output = {
-                        'ne_model': [np.zeros(shape=(len(LShellRange), len(altRange))), {'DEPEND_0': 'simLShell', 'DEPEND_1': 'simAlt', 'UNITS': 'm^-3', 'LABLAXIS': 'Electron Density'}],
-                        'q_total': [np.zeros(shape=(len(LShellRange), len(altRange))), {'DEPEND_0': 'simLShell', 'DEPEND_1':'simAlt', 'UNITS': 'm^-3s^-1', 'LABLAXIS': 'qtot'}],
-                         'alpha_recomb_total': [np.zeros(shape=(len(LShellRange), len(altRange))), {'DEPEND_0': 'simLShell', 'DEPEND_1':'simAlt', 'UNITS': 'm^3s^-1',  'LABLAXIS': 'Recombination Rate'}],
-                        'simLShell': deepcopy(data_dict_plasma['simLShell']),
-                        'simAlt': deepcopy(data_dict_plasma['simAlt'])
+    # prepare the output
+    data_dict_output = {'ni_slice_high': [np.zeros(shape=(len(data_dict_LShell_high['L-Shell'][0]))), {'DEPEND_0': 'Epoch_high'}],
+                        'Epoch_high': data_dict_LShell_high['Epoch'],
+                        'ni_slice_low': [np.zeros(shape=(len(data_dict_LShell_low['L-Shell'][0]))), {'DEPEND_0': 'Epoch_low'}],
+                        'Epoch_low': data_dict_LShell_low['Epoch'],
                         }
 
+    ####################################
+    # --- SAMPLE THE MODEL AT ACESII ---
+    ####################################
 
-    ############################
-    # --- RECOMBINATION RATE ---
-    ############################
-    model = schunkNagy2009()
-    alpha_total, alpha_profiles = model.calcRecombinationRate(altRange=altRange, data_dict_plasma=data_dict_plasma)
-    data_dict_output['alpha_recomb_total'][0] = alpha_total/(np.power(stl.cm_to_m,3)) # convert form cm^3/s to m^3/s
-    for idx, ionNam in enumerate(plasmaToggles.wIons):
-        data_dict_output = {**data_dict_output,
-                            **{f'alpha_recomb_{ionNam}':[alpha_profiles[idx], {'DEPEND_0': 'simLShell', 'DEPEND_1':'simAlt', 'UNITS': 'm^3s^-1',  'LABLAXIS': f'Recombination Rate {ionNam}'}]}}
+    stl.prgMsg('Sampling High Flyer')
+    for idx1, tme in enumerate(data_dict_LShell_high['Epoch'][0]):
 
-
-    # model = vickrey1982()
-    # alpha_total, alpha_profiles = model.calcRecombinationRate(altRange=deepcopy(data_dict_spatial['grid_alt'][0]))
-    # data_dict_output['alpha_recomb'][0] = alpha_total / (np.power(stl.cm_to_m, 3))  # convert form cm^3/s to m^3/s
+        # find where in the simulation data each point is closest
+        Lshell_idx = np.abs(data_dict_conductivity['simLShell'][0] - data_dict_LShell_high['L-Shell'][0][idx1]).argmin()
+        alt_idx = np.abs(data_dict_conductivity['simAlt'][0] - data_dict_LShell_high['Alt'][0][idx1]).argmin()
+        data_dict_output['ni_slice_high'][0][idx1] = data_dict_conductivity['ne_total'][0][Lshell_idx][alt_idx]
+    stl.Done(start_time)
 
 
-    ################################
-    # --- --- --- --- --- --- --- --
-    # --- LOOP THROUGH BEAM DATA ---
-    # --- --- --- --- --- --- --- --
-    ################################
-    for idx1, LShell in tqdm(enumerate(data_dict_output['simLShell'][0])):
+    stl.prgMsg('Sampling Low Flyer')
+    for idx1, tme in enumerate(data_dict_LShell_low['Epoch'][0]):
+        # find where in the simulation data each point is closest
+        Lshell_idx = np.abs(data_dict_conductivity['simLShell'][0] - data_dict_LShell_low['L-Shell'][0][idx1]).argmin()
+        alt_idx = np.abs(data_dict_conductivity['simAlt'][0] - data_dict_LShell_low['Alt'][0][idx1]).argmin()
+        data_dict_output['ni_slice_low'][0][idx1] = data_dict_conductivity['ne_total'][0][Lshell_idx][alt_idx]
+    stl.Done(start_time)
 
-        # get the flux data for the specific L-Shell
-        dat_idx = np.abs(data_dict_LShell['L-Shell'][0] - LShell).argmin()
-        varPhi_E_parallel_keV = deepcopy(data_dict_flux['varPhi_E_Parallel'][0][dat_idx])/1000
-        Energy_keV = deepcopy(data_dict_flux['Energy'][0])/1000
-
-        # CHOOSE THE MODEL
-        model = fang2010(altRange=altRange,
-                         Tn = deepcopy(data_dict_neutral['Tn'][0][idx1]),
-                         m_eff_n = deepcopy(data_dict_neutral['m_eff_n'][0][idx1]),
-                         rho_n = deepcopy(data_dict_neutral['rho_n'][0][idx1]),
-                         inputEnergies=Energy_keV,
-                         varPhi_E=varPhi_E_parallel_keV)
-
-        q_profiles, q_total = model.ionizationRate()  # in cm^-3 s^-1
-        data_dict_output['q_total'][0][idx1] = q_total*np.power(stl.cm_to_m,3) # convert to m^-3 s^-1
-
-        ##################################
-        # --- ELECTRON DENSITY (MODEL) ---
-        ##################################
-        data_dict_output['ne_model'][0][idx1] = np.sqrt(deepcopy(data_dict_output['q_total'][0][idx1]) / deepcopy(data_dict_output['alpha_recomb_total'][0][idx1]))  # in m^-3
 
 
     #####################
@@ -101,5 +75,7 @@ def generateIonizationRecomb():
 
         data_dict_output[key][1] = newAttrs
 
-    outputPath = rf'{ionizationRecombToggles.outputFolder}\ionization_rcomb.cdf'
+    outputPath = rf'C:\Data\physicsModels\ionosphere\special\ACESII_model_slice.cdf'
     outputCDFdata(outputPath, data_dict_output)
+
+ACESII_model_slice()
